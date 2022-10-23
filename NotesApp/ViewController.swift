2ccfed118f9ca25d929c39af2fdc32450e223b1b
Hau Hoang <hauhoangduc@gmail.com>
2022-10-23 15:02:12 -0700

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Hau Hoang on 10/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
