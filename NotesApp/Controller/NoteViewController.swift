a18886a510ba05c21164269fc124c2229b1394ea
Hau Hoang <hauhoangduc@gmail.com>
2022-10-23 16:09:31 -0700

    Notes iOS Application Without Database Setup

---
+++ NotesApp/Controller/NoteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NoteViewController.swift
+//  NotesApp
+//
+//  Created by Hau Hoang on 10/23/22.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+    }
+    
+}
